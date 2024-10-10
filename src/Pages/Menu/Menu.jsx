7a8404970495c379a1e3fe095a47ded1b4b65d16
Eftekhar Ahmed Shishir <eftekharahmedshishir@gmail.com>
2024-10-11 01:37:37 +0600

    offered component added

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,14 +1,31 @@
 import { Helmet} from 'react-helmet-async';
 import Cover from '../Shared/Cover/Cover';
 import menuImg from "../../../src/assets/menu/banner3.jpg"
-import PopularMenu from '../Home/PopularMenu/PopularMenu';
+import useMenu from '../../hooks/useMenu';
+import SectionTitle from '../../Components/SectionTitle/SectionTitle';
+import MenuCategory from './MenuCategory/MenuCategory';
+
 const Menu = () => {
+    const [menu] = useMenu();
+    const dessert = menu.filter(item => item.category === 'dessert')
+    const salad = menu.filter(item => item.category === 'salad')
+    const pizza = menu.filter(item => item.category === 'pizza')
+    const soup = menu.filter(item => item.category === 'soup')
+    const offered = menu.filter(item => item.category === 'offered')
+
     return (
         <div>
            <Helmet>
             <title>Harvest&Hearth| Menu</title>
            </Helmet>
            <Cover img={menuImg}title="our menu" ></Cover>
+           <SectionTitle
+           subHeading="Don't Miss"
+           heading="Today's Offer"
+           ></SectionTitle>
+           <MenuCategory
+           items={offered}
+           ></MenuCategory>
            
         </div>
     );
