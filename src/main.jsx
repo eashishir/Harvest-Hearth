3375c19d9547edb707b7847eb0c633951caa3f71
Eftekhar Ahmed shishir <108192348+eashishir@users.noreply.github.com>
2025-05-06 03:07:38 +0600

    login page setup and signup page

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,18 @@ import {
 } from "react-router-dom";
 import { router } from './Routes/Route';
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProviders from './providers/AuthProviders';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
+   <AuthProviders>
+   <HelmetProvider>
       <div className='max-w-screen-xl mx-auto'>
         <RouterProvider router={router} />
       </div>
     </HelmetProvider>
+   </AuthProviders>
+
   </StrictMode>,
 )
